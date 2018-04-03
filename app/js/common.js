c2b1d8ee4633cab06aebbcedd440238741bf768c
sergey-kras <vip.41243@gmail.com>
2018-04-03 18:02:43 +0700

    Added video without adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -42,6 +42,7 @@ $('.catalog_items').owlCarousel({
     margin:25,
     responsiveClass:true,
     nav: true,
+    dots: false,
     responsive:{
         0:{
             items:1,
@@ -81,4 +82,48 @@ $('.reviews_items').owlCarousel({
             loop:true
         }
     }
+});
+
+
+//youtube script
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+onYouTubeIframeAPIReady = function () {
+    player = new YT.Player('player', {
+        height: '450',
+        width: '800',
+        videoId: '3oPUVdduGPc',  // youtube video id
+        playerVars: {
+            'autoplay': 0,
+            'rel': 0,
+            'showinfo': 0
+        },
+        events: {
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+var p = document.getElementById ("player");
+$(p).hide();
+
+var t = document.getElementById ("thumbnail");
+t.src = "https://pp.userapi.com/c845420/v845420213/1b0c9/GrpNlFEr1sM.jpg";
+
+onPlayerStateChange = function (event) {
+    if (event.data == YT.PlayerState.ENDED) {
+        $('.start-video').fadeIn('normal');
+    }
+}
+
+$(document).on('click', '.start-video', function () {
+    $(this).hide();
+    $("#player").show();
+    $("#thumbnail_container").hide();
+    player.playVideo();
 });
